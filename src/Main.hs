b7713673e7f6179b282db00f7ffef89c828fd6cb
Stoyan Delev <s.delev@elements.nl>
2017-05-07 14:25:28 +0200

    Add twitter shit

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,30 @@ import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
 import Web.Scotty
+import Web.Authenticate.OAuth
+import Web.Twitter.Feed
+import Network.Wai.Middleware.RequestLogger
+import qualified Web.Twitter.Types as T
+import Control.Monad.IO.Class (liftIO)
+import Data.List
+import Data.Text.Lazy (pack, Text)
+
 
 data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
 instance ToJSON User
 instance FromJSON User
+
+
+myoauth :: OAuth
+myoauth = newOAuth
+    { oauthServerName     = "api.twitter.com"
+    , oauthConsumerKey    = ""
+    , oauthConsumerSecret = ""
+    }
+
+mycred :: Credential
+mycred = newCredential ""
+                       ""
 
 bob :: User
 bob = User { userId = 1, userName = "bob" }
@@ -22,13 +42,26 @@ allUsers = [bob, jenny]
 matchesId :: Int -> User -> Bool
 matchesId id user = userId user == id
 
+takeTwitBody :: [T.SimpleTweet] -> [String]
+takeTwitBody  = map (\twit -> T.body twit)
+
+twitsBodyToText :: [String] -> String
+twitsBodyToText  = intercalate " !!!<br /> "
+
+
+
 main = do
   putStrLn "Starting Server..."
 
   scotty 3001 $ do
+    middleware logStdoutDev
     get "/hello/:name" $ do
       name <- param "name"
-      text ("hello " <> name <> "!")
+      result <- liftIO $ timeline myoauth mycred 20 False name
+      case result of
+        Left msg -> text "ERROR"
+        Right k -> html $ pack $ twitsBodyToText $ takeTwitBody k
+        --twitsBodyToText $ takeTwitBody k
 
     get "/users" $ do
       json allUsers
@@ -36,6 +69,7 @@ main = do
     get "/users/:id" $ do
       id <- param "id"
       json $ head $ filter (matchesId id) allUsers
+
 
     -- assignment: post user and print it out
     post "/users" $ do
